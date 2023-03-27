3d2a07ef982d9ffffbac6c6716641641257d7c61
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-03-27 14:46:49 +0300

    crud backoffice product

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -29,5 +31,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Categorie', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Produit', 'fas fa-tag', Product::class);
     }
 }
